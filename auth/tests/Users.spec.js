fabae66e27d88f8a57d555d9403eb7f4c7e5d0e8
Noaman Ahmed <nauman.h@allshorevirtualstaffing.com>
2022-06-02 23:49:12 +0500

    Removed Errors and warnings

--- auth/tests/Users.spec.js
+++ auth/tests/Users.spec.js
@@ -1,36 +1,49 @@
-
 let supertest = require("supertest");
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
 require("dotenv").config({ path: ".env.test", debug: true });
 const http = require("http");
-let { initDb, closeConnection, dropDatabase , dropCollection} = require("../database/init");
+let {
+  initDb,
+  closeConnection,
+  dropDatabase,
+  dropCollection,
+} = require("../database/init");
 let { initAcl } = require("../acl/init");
 let { boot, registerRoutes } = require("../app");
 
 const mockResponse = () => {
-    const res = {};
-    res.status = jest.fn().mockReturnValue(res);
-    res.json = jest.fn().mockReturnValue(res);
-    return res;
+  const res = {};
+  res.status = jest.fn().mockReturnValue(res);
+  res.json = jest.fn().mockReturnValue(res);
+  return res;
 };
 
-const mockRequest = (bodyData) => {    
-    return {
-        body: bodyData,
-    };
+const mockRequest = (bodyData) => {
+  return {
+    body: bodyData,
+  };
 };
-
 
 const UserService = require("../services/UserService");
-let UserServiceObj = new UserService;
-
+let UserServiceObj = new UserService();
 
 const AuthService = require("../services/AuthService");
-let AuthServiceObj = new AuthService;
+let AuthServiceObj = new AuthService();
 
 describe("Users ", () => {
   let server;
   let request;
+
+  beforeAll(async () => {
+    initDb();
+    await initAcl();
+  });
+
+  afterAll(async () => {
+    await dropDatabase();
+    // await dropCollection('users');
+    closeConnection();
+  });
 
   beforeAll(done => {
     server = http.createServer(registerRoutes());
@@ -39,23 +52,8 @@ describe("Users ", () => {
   });
 
   afterAll(done => {
-    server.close(done);    
+    server.close(done);
   });
-
-  beforeAll(async () => {
-    initDb();
-    await initAcl();
-  });
-
-  afterAll(async () => {
-    // await dropDatabase();
-    // await dropCollection('users');    
-    closeConnection();
-
-  });
-
-
-
 
   test("login route is 400 without username and password", async () => {
     // Arrange
@@ -70,55 +68,55 @@ describe("Users ", () => {
   test("login route with correct username and password works", async () => {
     // Arrange
     let req = {
-        first_name : faker.name.firstName(), 
-        last_name : faker.name.lastName(), 
-        email : faker.internet.email().toLocaleLowerCase(),
-        password : faker.internet.password()
-    }
-    
-    let user = await AuthServiceObj.createUser(mockRequest(req),mockResponse())
-    
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email().toLocaleLowerCase(),
+      password: faker.internet.password(),
+    };
+
+    let user = await AuthServiceObj.createUser(
+      mockRequest(req),
+      mockResponse()
+    );
+
     // Act
     const response = await request.post("/api/auth/login").send({
       email: req.email,
       password: req.password,
     });
 
-    
     // Assert
     expect(response.statusCode).toBe(200);
     expect(response.header["content-type"]).toMatch(/json/);
-    expect(response.body.hasOwnProperty('email')).toBeTruthy();
-    expect(response.body.hasOwnProperty('token')).toBeTruthy();
-    expect(response.body.hasOwnProperty('role')).toBeTruthy();
-    expect(response.body.hasOwnProperty('permissions')).toBeTruthy();
-    
-
-  });  
+    expect(response.body.hasOwnProperty("email")).toBeTruthy();
+    expect(response.body.hasOwnProperty("token")).toBeTruthy();
+    expect(response.body.hasOwnProperty("role")).toBeTruthy();
+    expect(response.body.hasOwnProperty("permissions")).toBeTruthy();
+  });
 
   test("login route with incorrect username and password gives 401", async () => {
-    
     // Arrange
     let req = {
-      first_name : faker.name.firstName(), 
-      last_name : faker.name.lastName(), 
-      email : faker.internet.email().toLocaleLowerCase(),
-      password : faker.internet.password()
-    }
-    
-    let user = await AuthServiceObj.createUser(mockRequest(req),mockResponse())
-    
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email().toLocaleLowerCase(),
+      password: faker.internet.password(),
+    };
+
+    let user = await AuthServiceObj.createUser(
+      mockRequest(req),
+      mockResponse()
+    );
+
     // Act
     const response = await request.post("/api/auth/login").send({
       email: req.email,
       password: faker.internet.password(),
     });
-    
+
     // Assert
     expect(response.statusCode).toBe(401);
-
   });
-
 
   test("register route is 400 without any details", async () => {
     // Arrange
@@ -133,26 +131,23 @@ describe("Users ", () => {
   test("register route works with parameters", async () => {
     // Arrange
     let req = {
-      first_name : faker.name.firstName(), 
-      last_name : faker.name.lastName(), 
-      email : faker.internet.email().toLocaleLowerCase(),
-      password : faker.internet.password()
-    }
-  
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email().toLocaleLowerCase(),
+      password: faker.internet.password(),
+    };
+
     // Act
     const response = await request.post("/api/auth/register").send(req);
-  
-    
+
     // Assert
     expect(response.statusCode).toBe(201);
     expect(response.header["content-type"]).toMatch(/json/);
-    expect(response.body.hasOwnProperty('email')).toBeTruthy();
-    expect(response.body.hasOwnProperty('token')).toBeTruthy();
-    expect(response.body.hasOwnProperty('role')).toBeTruthy();
-    expect(response.body.hasOwnProperty('permissions')).toBeTruthy();
-    
-
-  });  
+    expect(response.body.hasOwnProperty("email")).toBeTruthy();
+    expect(response.body.hasOwnProperty("token")).toBeTruthy();
+    expect(response.body.hasOwnProperty("role")).toBeTruthy();
+    expect(response.body.hasOwnProperty("permissions")).toBeTruthy();
+  });
 
   test("List all users", async () => {
     // Arrange
@@ -163,97 +158,91 @@ describe("Users ", () => {
     // Assert
     expect(response.statusCode).toBe(200);
     expect(response.header["content-type"]).toMatch(/json/);
-    
   });
 
   test("Get a single user", async () => {
-    // Arrange    
+    // Arrange
     let userObj = {
-      first_name : faker.name.firstName(), 
-      last_name : faker.name.lastName(), 
-      email : faker.internet.email().toLocaleLowerCase(),
-      password : faker.internet.password(),
-      role: await UserServiceObj.getDefaultUserRole()
-    }
-  
-    const userModel = await UserServiceObj.User.create(userObj)
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email().toLocaleLowerCase(),
+      password: faker.internet.password(),
+      role: await UserServiceObj.getDefaultUserRole(),
+    };
+
+    const userModel = await UserServiceObj.User.create(userObj);
 
     // Act
-    const response = await request.get("/api/users/"+userModel._id);
+    const response = await request.get("/api/users/" + userModel._id);
 
     // Assert
     expect(response.statusCode).toBe(200);
     expect(response.header["content-type"]).toMatch(/json/);
-    expect(response.body.hasOwnProperty('email')).toBeTruthy();    
-    expect(response.body.hasOwnProperty('role')).toBeTruthy();
-    expect(response.body.hasOwnProperty('permissions')).toBeTruthy();
-    
-
+    expect(response.body.hasOwnProperty("email")).toBeTruthy();
+    expect(response.body.hasOwnProperty("role")).toBeTruthy();
+    expect(response.body.hasOwnProperty("permissions")).toBeTruthy();
   });
 
   test("Create a new user", async () => {
     // Arrange
     let req = {
-        first_name : faker.name.firstName(), 
-        last_name : faker.name.lastName(), 
-        email : faker.internet.email().toLocaleLowerCase(),
-        password : faker.internet.password()
-    }
-    
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email().toLocaleLowerCase(),
+      password: faker.internet.password(),
+    };
+
     // Act
     const response = await request.post("/api/users").send(req);
-    
+
     // Assert
     expect(response.statusCode).toBe(201);
     expect(response.header["content-type"]).toMatch(/json/);
-    expect(response.body.hasOwnProperty('email')).toBeTruthy();    
-    expect(response.body.hasOwnProperty('role')).toBeTruthy();
-    expect(response.body.hasOwnProperty('permissions')).toBeTruthy();
-
+    expect(response.body.hasOwnProperty("email")).toBeTruthy();
+    expect(response.body.hasOwnProperty("role")).toBeTruthy();
+    expect(response.body.hasOwnProperty("permissions")).toBeTruthy();
   });
 
   test("Updates a user without sending password", async () => {
     // Arrange
     let userObj = {
-        first_name : faker.name.firstName(), 
-        last_name : faker.name.lastName(), 
-        email : faker.internet.email().toLocaleLowerCase(),
-        password : faker.internet.password()
-    }
-          
-    const userModel = await UserServiceObj.User.create(userObj)
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email().toLocaleLowerCase(),
+      password: faker.internet.password(),
+    };
 
-    userObj.first_name = faker.name.firstName()
-    userObj.last_name = faker.name.lastName()
+    const userModel = await UserServiceObj.User.create(userObj);
 
+    userObj.first_name = faker.name.firstName();
+    userObj.last_name = faker.name.lastName();
 
     // Act
     const response = await request.patch("/api/users").send(userObj);
-    
+
     // Assert    ;
     expect(response.statusCode).toBe(200);
     expect(response.header["content-type"]).toMatch(/json/);
     expect(response.body.first_name).toEqual(userObj.first_name);
     expect(response.body.last_name).toEqual(userObj.last_name);
-
   });
 
   test("Updates a user's password", async () => {
     // Arrange
     let userObj = {
-        first_name : faker.name.firstName(), 
-        last_name : faker.name.lastName(), 
-        email : faker.internet.email().toLocaleLowerCase(),
-        password : faker.internet.password()
-    }
-          
-    const userModel = await UserServiceObj.User.create(userObj)
-    
-    userObj.password = faker.internet.password()
+      first_name: faker.name.firstName(),
+      last_name: faker.name.lastName(),
+      email: faker.internet.email().toLocaleLowerCase(),
+      password: faker.internet.password(),
+    };
+
+    const userModel = await UserServiceObj.User.create(userObj);
+
+    userObj.password = faker.internet.password();
 
     // Act
     let response = await request.patch("/api/users").send(userObj);
-    
+
     // Assert;
     response = await request.post("/api/auth/login").send({
       email: userObj.email,
@@ -262,11 +251,9 @@ describe("Users ", () => {
 
     expect(response.statusCode).toBe(200);
     expect(response.header["content-type"]).toMatch(/json/);
-    expect(response.body.hasOwnProperty('email')).toBeTruthy();
-    expect(response.body.hasOwnProperty('token')).toBeTruthy();
-    expect(response.body.hasOwnProperty('role')).toBeTruthy();
-    expect(response.body.hasOwnProperty('permissions')).toBeTruthy();
-
+    expect(response.body.hasOwnProperty("email")).toBeTruthy();
+    expect(response.body.hasOwnProperty("token")).toBeTruthy();
+    expect(response.body.hasOwnProperty("role")).toBeTruthy();
+    expect(response.body.hasOwnProperty("permissions")).toBeTruthy();
   });
-
 });
