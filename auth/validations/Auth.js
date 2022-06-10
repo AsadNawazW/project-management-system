40a1ce6732dc813449aee4389f72a46454d7e1d2
Noaman Ahmed <nauman.h@allshorevirtualstaffing.com>
2022-06-11 01:37:48 +0500

    Refactor validations logic and add further error messages

--- auth/validations/Auth.js
+++ auth/validations/Auth.js
@@ -1,20 +1,63 @@
 // Load Validation imports
 import { check } from "express-validator";
 
+let firstNameValidation = check('first_name')
+.exists()
+.withMessage('first_name field must be present')
+.bail()
+.isLength({min:2,max:32})
+.bail()
+
+let lastNameValidation = check('last_name')
+.exists()
+.withMessage('last_name field must be present')
+.bail()
+.isLength({min:2,max:32})
+.bail()
+
+let emailValidation = check('email')
+.exists()
+.withMessage('email field must be present')
+.bail()
+.isEmail()
+.withMessage('email provided is not a valid email address')
+.bail()
+
+let passwordValidation = check('password')
+.exists()
+.withMessage('password field must be present')
+.bail()
+.isLength({min:8,max:32})
+.withMessage('password field must be greater than 8 characters and less than 32 characters')
+
+let accessTokenValidation =  check('access_token')
+.exists()
+.withMessage('access_token field must be present')
+.bail() 
+
+let roleNameValidation = check('name')
+.exists()
+.withMessage('name field is required to attach roles')
+.bail()
+.isLength({min:2,max:32})
+.withMessage('name field to specify the role must be greater than 2 characters and less than 32 characters')
+.bail()
+
 
 const registerValidator = [        
-        check('first_name','First Name is required').exists().bail().isLength({min:2,max:32}).bail(),
-        check('last_name','Last name is required!').exists().bail().isLength({min:2,max:32}).bail(),
-        check('email','Email is required').exists().bail().isEmail().bail(),
-        check('password','Password is required').exists().bail().isLength({min:8,max:32})
+    firstNameValidation,
+    lastNameValidation,
+    emailValidation,
+    passwordValidation
 ]
 
 const loginValidator = [
-    check('email','Email is required').exists().bail().isEmail().bail(),
-    check('password','Password is required').exists().bail().isLength({min:8,max:32})
+    emailValidation,
+    passwordValidation
 ]
 const refreshTokenValidator = [
-    check('access_token','Refresh token is required').exists().bail().isLength({min:8,max:32})    
+    accessTokenValidation,
+    roleNameValidation
 ]
 
 module.exports =  {
